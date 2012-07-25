54c24bda7c28a7a64fb6a2353ed99e71afecaff9
unknown <anilal.sambasivan@VDC01HPW0479.IN.valtech.com>
2012-07-25 15:25:26 +0530

    Form C Popups - First Commit

---
+++ FormC.Popups/FormC.Popups/Model/CustomerDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace FormC.Popups.Model
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
