4fc3357650968c9470fa74f0b075c6c74eef71d9
unknown <anilal.sambasivan@VDC01HPW0479.IN.valtech.com>
2012-07-25 13:52:38 +0530

    First Commit

---
+++ FormC.Popups/FormC.Popups/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using FormC.Popups.ViewModel;
+
+namespace FormC.Popups
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            var mainVm = new MainViewModel();
+            InitializeComponent();
+            this.DataContext = mainVm;
+        }
+    }
+}
